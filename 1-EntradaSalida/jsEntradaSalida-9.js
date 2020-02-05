19411c2ec1f3f84d3829e38a7f9f12cc0034e8a9
GJoaco <60699348+GJoaco@users.noreply.github.com>
2020-02-05 11:58:05 -0300

    Terminado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,22 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo
+	var sacarPorcentajeUno
+	var sacarPorcentajeDos
+	var resultado
+
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+	console.log(sueldo);
+
+	sacarPorcentajeUno=10
+	sacarPorcentajeDos=100
+
+	resultado=sueldo + sueldo * sacarPorcentajeUno/sacarPorcentajeDos;
+	
+	document.getElementById('resultado').value=resultado;
+	console.log(resultado);
 
 	
 }
