b0d6a56b6213341cb8c74f320d287426eb983143
GJoaco <60699348+GJoaco@users.noreply.github.com>
2020-02-06 03:30:06 -0300

    Terminados

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio
+	var descuento
+	var precioFinal
 
+	precio=prompt("Introduzca precio ","Precio");
+	precio=parseInt(precio);
+	descuento=prompt("Introduzca descuento","Descuento");
+	descuento=parseInt(descuento);
+	precioFinal=precio-precio*descuento/100;
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+	var ancho
+	var largo
+	var lados
+	var perimetro
+
+	ancho=prompt("Indrotuzca ancho","Ancho");
+	ancho=parseInt(ancho);
+	largo=prompt("Indrotuzca largo","Largo");
+	largo=parseInt(largo);
+
+	lados=ancho+largo;
+	perimetro=lados*2
+
+	alert(perimetro);
 
 }
