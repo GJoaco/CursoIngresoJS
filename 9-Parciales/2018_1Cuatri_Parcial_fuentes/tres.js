0c6fedb57ee48dda71ad065a5d2dd032da60393a
alumno <alumno>
2020-03-03 10:38:22 -0300

    Ejercicio parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,13 +1,119 @@
+/*
+b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en
+una sola reserva.
+
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva*/
+
 function mostrar()
 {
-	var precio
-	var descuento
-	var precioFinal
+	var nombre;
+	var cantidadDePersonas;
+	var cantidadDeDias;
+	var formaDePago;
+	var respuesta;
+	var maximoDePersonas;
+	var maximoDePersonasNombres;
+	var contador;
+	var acumulador;
+	var promedio;
+	var maximoDeDias;
+	var maximoDeDiasCantidadDePersonas;
+	var contadorQR;
+	var contadorTarjeta;
+	var contadorEfectivo;
+	var formaDePagoMasUtilizada;
 
-	precio=prompt("Precio");
-	descuento=prompt("Descuento");
 
-	precioFinal=precio- precio*descuento/100;
+	respuesta="si";
+	contador=0;
+	acumulador=0;
+	contadorQR=0;
+	contadorEfectivo=0;
+	contadorTarjeta=0;
 
-	document.getElementById('elPrecioFinal').value=precioFinal
+	while(respuesta=="si")
+	{
+		nombre=prompt("Ingrese nombre");
+
+		do
+		{
+			cantidadDePersonas=prompt("Ingrese cantidad de personas");
+			cantidadDePersonas=parseInt(cantidadDePersonas);
+		}while(isNaN(cantidadDePersonas) || cantidadDePersonas<1);
+
+		do
+		{
+			cantidadDeDias=prompt("Ingrese cantidad de dias");
+			cantidadDeDias=parseInt(cantidadDeDias);
+		}while(isNaN(cantidadDeDias) || cantidadDeDias<1);
+
+		do
+		{
+			formaDePago=prompt("Ingrese forma de pago");
+		}while(!isNaN(formaDePago) || formaDePago!="qr" && formaDePago!="tarjeta" && formaDePago!="efectivo");
+
+		console.log("Cantidad de personas "+cantidadDePersonas);
+		console.log("Cantidad de dias "+ cantidadDeDias);
+		console.log("Forma de pago "+formaDePago);
+
+		if(contador==0 || maximoDePersonas<cantidadDePersonas)
+		{
+			maximoDePersonas=cantidadDePersonas;
+			maximoDePersonasNombres=nombre;
+		}
+
+		if(contador==0 || maximoDeDias<cantidadDeDias)
+		{
+			maximoDeDias=cantidadDeDias;
+			maximoDeDiasCantidadDePersonas=cantidadDePersonas;
+		}
+
+		switch(formaDePago)
+		{
+			case "efectivo":
+				contadorEfectivo++;
+				break;
+			case "tarjeta":
+				contadorTarjeta++;
+				break;
+			default:
+				contadorQR++;
+				break;
+		}
+		
+		contador++;
+		respuesta=prompt("Desea continuar?");
+
+	}
+
+	if (contadorEfectivo>contadorTarjeta && contadorEfectivo>contadorQR) 
+	{
+		formaDePagoMasUtilizada="efectivo";
+	}
+	else
+	{
+		if(contadorQR>contadorTarjeta)
+		{
+			formaDePagoMasUtilizada="qr";
+		}
+		else
+		{
+			formaDePagoMasUtilizada="tarjeta";
+		}
+		
+	}	
+
+	promedio=acumulador/contador;
+	console.log("maximoDePersonasNombres"+maximoDePersonasNombres);
+	console.log("maximoDeDiasCantidadDePersonas"+maximoDeDiasCantidadDePersonas)
+	console.log("formaDePagoMasUtilizada"+formaDePagoMasUtilizada)
 }
